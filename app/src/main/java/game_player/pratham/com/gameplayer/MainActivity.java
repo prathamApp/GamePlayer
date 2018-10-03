d9d618910dca9b9a02355b1df3c374565be5c441
prathamApp <prathamApp>
2018-10-03 16:45:04 +0530

    Initial commit

---
+++ app/src/main/java/game_player/pratham/com/gameplayer/MainActivity.java
@@ -0,0 +1,13 @@
+package game_player.pratham.com.gameplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
